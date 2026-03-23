5cb0a76411e96fff4190b162842a5a204b768ba4
Olav Landsverk <olav.landsverk@gmail.com>
2026-03-23 14:55:45 +0100

    update

--- fpl/Fpl/Proofs.lean
+++ fpl/Fpl/Proofs.lean
@@ -2,13 +2,13 @@ def isEven (n : Nat) : Prop := ∃ n': Nat, Nat.add n' n' = n
 
 def isOdd (n : Nat) : Prop := ∃ n': Nat, (n' + n') + 1 = n
 
-def isEvenOrOdd (n : Nat) : Prop := isEven n ∨ isOdd n
-
 
 def oneIsOdd: isOdd 1 := sorry
 
 def twoIsEven: isEven 2 := sorry
 
-def threeIsEvenOrOdd: isEvenOrOdd 3 := sorry
+-- function that, given n, gives us the proof that n is either even or odd
+def isEvenOrOdd (n : Nat) : isEven n ∨ isOdd n := sorry
 
-def everyNumberIsEvenOrOdd : ∀ n , isEvenOrOdd n := sorry
+-- prove that every number is even or odd using the above function
+def everyNumberIsEvenOrOdd : ∀ n , isEven n ∨ isOdd n := sorry
